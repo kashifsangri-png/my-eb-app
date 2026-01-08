1570ffadeceba9aad967a2661c7085e7d3412841
kashifsangri-png <kashif.sangri@gmail.com>
2026-01-08 18:19:33 +0500

    Delete templates/test-app.py

--- templates/test-app.py
+++
@@ -1,8 +0,0 @@
-def test_simple_math():
-    # This is a dummy test to see if the pipeline works
-    assert 1 + 1 == 2
-
-def test_health_check():
-    # You can add more real tests here later
-    status = "ok"
-    assert status == "ok"
